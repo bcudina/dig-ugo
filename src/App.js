dae6a2c5b2231fe3b279daaa0b4cc04b09444669
bcudina <bozidar.cudina@gmail.com>
2019-11-22 23:11:02 +0100

    start 22112019

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Nav from "./component/navbar";
+import Home from "./component/home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Home />
+      <h1> hallo </h1>
+      <h4> druga linija </h4>
     </div>
   );
 }
