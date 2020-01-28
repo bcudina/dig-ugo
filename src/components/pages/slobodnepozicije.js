f09a2fb0bd1fb8e4faf9d43e63e60bfe71d33494
bcudina <bozidar.cudina@gmail.com>
2020-01-28 21:09:11 +0100

    28012020 samo update

--- src/components/pages/slobodnepozicije.js
+++ src/components/pages/slobodnepozicije.js
@@ -2,19 +2,24 @@ import React, { Component } from "react";
 import NewsCard from "../NewsCard";
 
 import { InfoConsumer } from "../context";
+import Title from "./Title";
 
 /* class */
 
 class slobodnePozicije extends Component {
   render() {
     return (
-      <InfoConsumer>
-        {value => {
-          return value.slobodne.map(item => {
-            return <NewsCard key={item.id} item={item} />;
-          });
-        }}
-      </InfoConsumer>
+      <div className="container">
+        <Title prvo="slobodne" drugo="Pozicije" />
+
+        <InfoConsumer>
+          {value => {
+            return value.slobodne.map(item => {
+              return <NewsCard key={item.id} item={item} />;
+            });
+          }}
+        </InfoConsumer>
+      </div>
     );
 
     //<div>Nažalost. trenutno nema slobodnoh pozicija</div>
